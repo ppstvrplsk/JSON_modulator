fb471665c0399592627a7b2824342e7b0ca31796
polykarpospolykarpidis <polykarpospolykarpidis@gmail.com>
2025-07-12 18:07:51 +0300

    first commit

---
+++ JSON_modulator.py
@@ -0,0 +1,47 @@
+from typing import Union
+import json
+import os
+
+class JSON_modulator:
+    def __init__(self, json):
+        self.json = json
+
+    def subjson_add(self, position: str, subjson:dict) -> bool:
+        pass
+
+    def listitem_append(self, position: str, item: Union[str | int | float]) -> bool:
+        pass
+
+    def listitem_delete(self, position: str, item) -> bool:
+        pass
+
+    def key_rename(self, position: str, key) -> bool:
+        pass
+
+    def value_rename(self, position: str, key_value: dict) -> bool:
+        pass
+
+
+
+
+
+
+if '__main__' == __name__:
+    path = os.path.join('examples','example1.json')
+    with open(path, mode='r', encoding='utf-8') as f:
+        json_obj = json.load(f)
+    mymod=JSON_modulator(json_obj)
+    mymod.key_remove('glossary^GlossDiv^GlossList', 'ID')
+
+    my_dict = {
+        'my_key1':'value1',
+        'my_key2': {
+            'my_key3': [2,3,2]
+        }
+    }
+    mymod.subjson_add('glossary^GlossDiv^GlossList', my_dict)
+    mymod.listitem_append('glossary^GlossDiv^GlossList^GlossEntry^GlossDef^GlossSeeAlso', "ABC")
+    mymod.listitem_delete('glossary^GlossDiv^GlossList^GlossEntry^GlossDef^GlossSeeAlso', "GML")
+    mymod.key_rename('glossary^GlossDiv^GlossList', 'new_key')
+    mymod.value_rename('glossary^GlossDiv^GlossList', 'new_value')
+
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # JSON_modulator
+
+
+```python
+mymod.key_remove('glossary^GlossDiv^GlossList', 'ID')
+
+my_dict = {
+    'my_key1':'value1',
+    'my_key2': {
+        'my_key3': [2,3,2]
+    }
+}
+
+mymod.subjson_add('glossary^GlossDiv^GlossList', my_dict)
+
+
+mymod.listitem_append('glossary^GlossDiv^GlossList^GlossEntry^GlossDef^GlossSeeAlso', "ABC")
+mymod.listitem_delete('glossary^GlossDiv^GlossList^GlossEntry^GlossDef^GlossSeeAlso', "GML")
+mymod.key_rename('glossary^GlossDiv^GlossList', 'new_key')
+mymod.value_rename('glossary^GlossDiv^GlossList', 'new_value')
+```
+
+# Extra
+Support * wildcard for a term in position
+```python
+mymod.value_rename('glossary^*^GlossList', 'new_value')
+mymod.value_rename('glossary^*^*^GlossList', 'new_value')
+mymod.value_rename('glossary^**^GlossList', 'new_value')
+```
